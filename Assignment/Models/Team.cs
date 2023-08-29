3f7fc0b4c05957f53f0b988619f0337b83fcaaa2
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-08-29 17:37:13 +0530

    my commit

---
+++ Assignment/Models/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment.Models
+{
+
+    [Table("Team")]
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string TeamName { get; set; }
+        [Required]
+
+        public int NOWC { get; set; }
+    }
+}
